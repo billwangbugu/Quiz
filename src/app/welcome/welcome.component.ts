d11d640ac6b655ddf890d02649739eb940ff5871
bugu wang <bugu.wang@gmail.com>
2020-07-08 10:25:18 -0400

    upgrade to angular 8

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-welcome",
   templateUrl: "./welcome.component.html",
-  styleUrls: ["./welcome.component.css"]
+  styleUrls: ["./welcome.component.css"],
 })
 export class WelcomeComponent implements OnInit {
   constructor() {}
-  name: string = "Angular 7";
+  name: string = "Angular 8";
 
   ngOnInit() {}
 }
